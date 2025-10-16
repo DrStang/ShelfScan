51a9d00c6c8b088947edc21232e6fcb5a84b5a62
DrStang <48525932+DrStang@users.noreply.github.com>
2025-10-16 13:31:01 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -534,10 +534,9 @@ const topThreeBooks = displayBooks.slice(0, 3);
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
                             version="1.1"
-                            width="256"
-                            height="256"
+                            width="24"
+                            height="24"
                             viewBox="0 0 24 24"
-                            className="w-4 h-4 sm:w-5 sm:h-5 flex-shrink-0"
                             xmlSpace="preserve"
                           >
                             <g
