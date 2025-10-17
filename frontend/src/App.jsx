d843420a4ab0540c213150824967b8b0edfa57b5
DrStang <48525932+DrStang@users.noreply.github.com>
2025-10-17 11:12:13 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -553,7 +553,7 @@ const topThreeBooks = displayBooks.slice(0, 3);
                             rel="noopener noreferrer"
                             className="inline-flex items-center justify-center"
                             >
-                           <img src={amazonImage} alt="Buy on Amazon" className="h-36 w-auto sm:h-40 hover:opacity-80 transition-opacity" />
+                           <img src={amazonImage} alt="Buy on Amazon" className="h-40 w-auto sm:h-44 hover:opacity-80 transition-opacity" />
                           
                           </a>
                         {book.infoLink && (
