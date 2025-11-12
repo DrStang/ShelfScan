e5658a8ab770603e6fd1ef669c0e94f25c839bfe
DrStang <48525932+DrStang@users.noreply.github.com>
2025-11-12 17:24:37 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -272,7 +272,7 @@ const topThreeBooks = displayBooks.slice(0, 3);
                     
                     {/* User Email - Hidden on mobile */}
                     <button
-                      onClick=() => setShowProfileModal(true)}
+                      onClick={() => setShowProfileModal(true)}
                       className="p-2 sm:px-3 sm:py-2 lg:px-4 bg-indigo-50 rounded-lg"
                       title="User Profile"
                     >
